a596da26c3df49d745d4e24f0c1564cd5fed6585
Amar Gill <amardeep@clearbanc.com>
2023-07-09 19:10:24 -0400

    implement binarysearch

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,28 @@
+/**
+ * array must be sorted!
+ * [low, high) <- inclusive of low, exclusive of high
+ */
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    while (low < high) {
+        let mid = Math.floor(low + (high - low) / 2);
+
+        const v = haystack[mid];
+
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            // midpoint value greater than target
+            // exclude top half
+            high = mid;
+        } else {
+            // midpoint value less than target
+            // exclude bottom half
+            low = mid + 1;
+        }
+    }
+
+    return false;
 }
