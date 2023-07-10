bcfbd2f556a42bca6619906aa3ffe8ac70b13e8d
Amar Gill <amardeep@clearbanc.com>
2023-07-09 22:12:53 -0400

    implement bubblesort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,15 @@
 export default function bubble_sort(arr: number[]): void {
+    let L = arr.length;
 
+    while (L > 0) {
+        for (let index = 0; index < L; index++) {
+            if (arr[index] > arr[index + 1]) {
+                const left = arr[index];
+                const right = arr[index + 1];
+                arr[index] = right;
+                arr[index + 1] = left;
+            }
+        }
+        L -= 1;
+    }
 }
