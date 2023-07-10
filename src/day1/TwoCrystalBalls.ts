5642127729095160d72aeaf7733e25a8a23bcdbc
Amar Gill <amardeep@clearbanc.com>
2023-07-09 21:23:48 -0400

    implement TwoCrystalBalls - different way

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,28 @@
+/**
+ * O(sqrt(N))
+ */
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump = Math.floor(Math.sqrt(breaks.length));
 
+    let low = 0;
+    let high = jump;
+
+    // if breaks[high] = true
+    // walk back to low
+    // and perform linear search until true
+    // else high = high + jump and low = low + jump
+
+    while (high <= breaks.length) {
+        if (breaks[high]) {
+            for (let index = low; index < high; index++) {
+                if (breaks[index]) {
+                    return index;
+                }
+            }
+        }
+        low += jump;
+        high += jump;
+    }
+
+    return -1;
 }
