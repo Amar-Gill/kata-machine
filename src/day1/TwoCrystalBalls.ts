8d3daf746ff1252031cd45bea28e2cd665c0a607
Amar Gill <amar.s.gill@outlook.com>
2024-08-15 22:41:08 -0400

    TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
+// [ f, f, f, f, t, t, t, t]
+// [ f, f, f, f, f, f, t, t]
+// O(sqrt(N))
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+  let i = jumpAmount;
+
+  for (; i < breaks.length; i += jumpAmount) {
+    if (breaks[i]) {
+      break;
+    }
+  }
+  i -= jumpAmount;
+
+  for (let j = 0; j <= jumpAmount; ++j, ++i) {
+    if (breaks[i]) {
+      return i
+    }
+  }
+
+  return -1
 }
