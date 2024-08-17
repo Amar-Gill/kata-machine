3a85885209f41e581736b900ea19b9bcb428cfb7
Amar Gill <amar.s.gill@outlook.com>
2024-08-17 01:46:22 -0400

    Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,58 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>
+  private tail?: Node<T>
 
-    
 
-    constructor() {
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
+
+  // O(1)
+  enqueue(item: T): void {
+    this.length++
+
+    const node = { value: item }
+
+    if (!this.tail) {
+      this.tail = this.head = node;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = node;
+    this.tail = node;
+  }
 
-}
-    deque(): T | undefined {
+  // O(1)
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
 
-}
-    peek(): T | undefined {
+    this.length--
 
-}
+    const head = this.head;
+
+    this.head = this.head.next;
+
+    // free memory
+    head.next = undefined
+
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+
+    return head.value;
+  }
+
+  // O(1)
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
