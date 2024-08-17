eedf80d96bf2277be4c6548508a578713c375a36
Amar Gill <amar.s.gill@outlook.com>
2024-08-17 16:32:04 -0400

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,52 @@
+type Node<T> = {
+  value: T,
+  prev?: Node<T>
+}
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>
 
-    
 
-    constructor() {
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
+
+  // O(1)
+  push(item: T): void {
+    this.length++;
+
+    const node: Node<T> = { value: item }
+
+    if (this.head) {
+      node.prev = this.head;
     }
 
-    push(item: T): void {
+    this.head = node;
+  }
 
-}
-    pop(): T | undefined {
+  // O(1)
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1);
 
-}
-    peek(): T | undefined {
+    if (this.length === 0) {
+      const head = this.head;
 
-}
+      this.head = undefined;
+
+      return head?.value;
+    }
+
+    const head = this.head;
+
+    this.head = head?.prev;
+
+    return head?.value;
+  }
+
+  // O(1)
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
