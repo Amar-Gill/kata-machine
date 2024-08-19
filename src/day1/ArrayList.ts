ebb60c83d91c31c8c8bf00bca16e6329173ab542
Amar Gill <amar.s.gill@outlook.com>
2024-08-19 18:29:07 -0400

    ArrayList.append

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,36 @@
 export default class ArrayList<T> {
-    public length: number;
+  public length: number;
+  private a: Array<T | undefined>;
 
-    
+  constructor(length: number) {
+    this.length = length;
+    this.a = new Array(length).fill(undefined)
+  }
 
-    constructor() {
+  prepend(item: T): void { }
+
+  insertAt(item: T, idx: number): void { }
+
+  // O(1) if within capacity (initial length)
+  // otherwise O(N) if we need to grow the array because each el must be copied into new array
+  append(item: T): void {
+    if (this.a.length === this.length) {
+      this.a = this.a.concat(new Array(this.length).fill(undefined))
     }
 
-    prepend(item: T): void {
+    this.length++;
 
-}
-    insertAt(item: T, idx: number): void {
+    this.a.push(item);
 
-}
-    append(item: T): void {
+  }
 
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+  remove(item: T): T | undefined {
+    return;
+  }
+  get(idx: number): T | undefined {
+    return;
+  }
+  removeAt(idx: number): T | undefined {
+    return;
+  }
 }
