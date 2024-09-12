028e54d0d0bfa434896de39f231d7ba24972f3f6
Amar Gill <amar.s.gill@outlook.com>
2024-09-12 19:51:12 -0400

    BTBFS - implement

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: BinaryNode<number>[] = [head]; // TODO use actual Queue implementation
 
+    while (q.length) {
+        const curr = q.shift();
+
+        if (curr?.value === needle) {
+            return true;
+        }
+
+        if (curr?.left) {
+            q.push(curr.left);
+        }
+
+        if (curr?.right) {
+            q.push(curr.right);
+        }
+    }
+
+    return false;
 }
+
