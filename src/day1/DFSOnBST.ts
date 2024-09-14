8d686dc3e16f1a9cf8e4295a58f7fff888d25385
Amar Gill <amar.s.gill@outlook.com>
2024-09-14 00:07:12 -0400

    DFSOnBST: add comments

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,4 +1,6 @@
 // Run time O(h) -- h = height of tree
+// worst case O(n) and best case is O(log(n)) -- because splitting search space in half each iteration
+// so O(h) is between O(n) and O(logn)
 export default function dfs(
     head: BinaryNode<number> | null,
     needle: number,
