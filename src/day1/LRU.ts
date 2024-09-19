524f8cd8eda14c7fdf118a4649db59591bff3db7
Amar Gill <amar.s.gill@outlook.com>
2024-09-18 23:22:20 -0400

    LRU - set up code

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,40 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
+function createNode<V>(value: V): Node<V> {
+    return { value };
+}
+
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
 
-    constructor() {
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>();
+        this.reverseLookup = new Map<Node<V>, K>();
     }
 
     update(key: K, value: V): void {
+        // does it exist?
+        // if it doesn't we need to insert
+        //    - check capacity and evict if over
+        // if it does we need to update to front of list
+        // and update the value
+    }
 
-}
     get(key: K): V | undefined {
+        // check cache for existence
+        // update value we found and move it to the front
+        // return value found or undefined if not exists
+    }
+}
 
-}
-}
