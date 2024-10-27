9c42a9e77c1db9100a77d2373720e512aef218ab
Amar Gill <amar.s.gill@outlook.com>
2024-10-27 01:45:04 -0400

    MinHeap: add comments

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -8,7 +8,9 @@ export default class MinHeap {
     }
 
     insert(value: number): void {
+        // 1. Add new value to end of tree
         this.data[this.length] = value;
+        // 2. Bubble up the new value to correct position
         this.heapifyUp(this.length);
         this.length++;
     }
@@ -19,6 +21,7 @@ export default class MinHeap {
             return -1;
         }
 
+        // 1. Save reference to min value at top of heap
         const out = this.data[0];
         this.length--;
 
@@ -27,8 +30,12 @@ export default class MinHeap {
             return out;
         }
 
+        // 2. Move max value at end of tree to root of tree
         this.data[0] = this.data[this.length];
+        // 3. Bubble down the value to the correct position
         this.heapifyDown(0);
+
+        // 4. Return the reference to min value that was saved
         return out;
     }
 
@@ -49,10 +56,16 @@ export default class MinHeap {
         const rV = this.data[rIdx];
         const v = this.data[idx];
 
+        // If left value >= right value and the target value is greater than right value
+        // Swap right value with target value
+        // Then bubble down the right node (which is now the target value)
         if (lV >= rV && v >= rV) {
             this.data[idx] = rV;
             this.data[rIdx] = v;
             this.heapifyDown(rIdx);
+            // Else if right value >= left value and the target value is greater than left value
+            // Swap left value with target value
+            // Then bubble down the left node (which is now the target value)
         } else if (rV >= lV && v >= lV) {
             this.data[idx] = lV;
             this.data[lIdx] = v;
@@ -65,10 +78,13 @@ export default class MinHeap {
             return;
         }
 
+        // get reference to target node and parent node
         const pIdx = this.parent(idx);
         const parentV = this.data[pIdx];
         const v = this.data[idx];
 
+        // if parent node > target node, swap the nodes
+        // then recursively bubble up from the parent node (which has been updated to target value)
         if (parentV > v) {
             this.data[idx] = parentV;
             this.data[pIdx] = v;
@@ -88,4 +104,3 @@ export default class MinHeap {
         return idx * 2 + 2;
     }
 }
-
